52a080c517c8879bb434b7775281320bc4da2336
Parth Sharma <97347045+PARTH-1712@users.noreply.github.com>
2024-12-20 23:31:26 +0530

    Time: 31 ms (45.71%) | Memory: 47.7 MB (25.22%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -3,44 +3,44 @@ public:
     int leastInterval(vector<char>& tasks, int n) {
         vector<int> mp(26, 0);
 
-        for(char &ch : tasks){
-            mp[ch-'A']++;
+        priority_queue<int>pq;
+
+        for(auto it:tasks){
+            mp[it-'A']++;
         }
 
-        int time = 0;
-
-        priority_queue<int> pq;
-
-        for(int i=0; i<26; i++ ){
-        if(mp[i] > 0){
-            pq.push(mp[i]);
-        }
+        for(int i=0; i<26; i++){
+            if(mp[i] >0){
+                pq.push(mp[i]); 
+            }
         }
 
+        int time =0;
         while(!pq.empty()){
+
             vector<int> temp;
 
-            for(int i = 1; i<=n+1; i++){
+            for(int i=1; i<=n+1; i++){
                 if(!pq.empty()){
-                    int freq = pq.top();
+                    int top = pq.top();
                     pq.pop();
-                    freq--;
-                    temp.push_back(freq);
+                    temp.push_back(top-1);
                 }
             }
-
-            for(int &f: temp){
-                if(f > 0){
-                    pq.push(f);
+            
+            for(auto it:temp){
+                if(it>0){
+                    pq.push(it);
                 }
             }
-
+            
             if(pq.empty()){
-                time += temp.size();
+                time += temp.size(); 
             }else{
                 time += n+1;
             }
         }
+
         return time;
     }
 };
